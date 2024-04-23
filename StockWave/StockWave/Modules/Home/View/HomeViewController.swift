5d4b4b7473d9485952edba0a3fccc3359399d13c
rauanium <a.spark69@gmail.com>
2024-04-23 20:33:08 +0500

    Testing API

--- StockWave/StockWave/Modules/Home/View/HomeViewController.swift
+++ StockWave/StockWave/Modules/Home/View/HomeViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import Kingfisher
 
 class HomeViewController: UIViewController {
+    
+    private lazy var image: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "road")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        setupViews()
     }
 
+    func setupViews() {
+        view.addSubview(image)
+        image.snp.makeConstraints { make in
+            make.size.equalTo(200)
+            make.center.equalToSuperview()
+        }
+        setupImage()
+    }
+    
+    func setupImage(){
+        let imageUrl = URL(string: "https://financialmodelingprep.com/image-stock/NVDA.png")
+        image.kf.setImage(with: imageUrl)
+    }
 
 }
 
